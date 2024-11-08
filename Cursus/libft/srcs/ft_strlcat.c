edff1fdd23dd4034e69af830a65ddd52592afc83
Wifons <williamfons6@gmail.com>
2024-11-08 19:52:00 +0100

    update

--- Cursus/libft/srcs/ft_strlcat.c
+++ Cursus/libft/srcs/ft_strlcat.c
@@ -6,25 +6,25 @@
 /*   By: jewtwo <jewtwo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 18:57:25 by wifons            #+#    #+#             */
-/*   Updated: 2024/11/08 19:12:57 by jewtwo           ###   ########.fr       */
+/*   Updated: 2024/11/08 19:51:44 by jewtwo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft.h"
 
-size_t ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
 	size_t	dst_len;
 	size_t	src_len;
+	size_t	i;
 
 	src_len = ft_strlen(src);
 	if (!dst && size == 0)
-        return src_len;
+		return (src_len);
 	dst_len = ft_strlen(dst);
-	i = 0;
 	if (size <= dst_len)
 		return (size + src_len);
+	i = 0;
 	while (src[i] && (dst_len + i) < (size - 1))
 	{
 		dst[dst_len + i] = src[i];
