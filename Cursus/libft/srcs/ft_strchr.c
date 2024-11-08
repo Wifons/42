2b6c93396f4d7626f3b12a87ff8a867ec4d3b6df
William Fons <wifons@c2r2p2.42perpignan.fr>
2024-11-08 20:07:37 +0100

    update ft_strchr

--- Cursus/libft/srcs/ft_strchr.c
+++ Cursus/libft/srcs/ft_strchr.c
@@ -6,11 +6,20 @@
 /*   By: wifons <wifons@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 20:03:09 by wifons            #+#    #+#             */
-/*   Updated: 2024/11/08 20:03:35 by wifons           ###   ########.fr       */
+/*   Updated: 2024/11/08 20:07:27 by wifons           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/ft.h"
+
 char	*ft_strchr(const char *s, int c)
 {
-
+	while (*s)
+	{
+		if (*s == (char)c)
+			return (char *)s++;
+	}
+	if (c == '\0')
+		return (char *)s;
+	return (NULL);
 }
