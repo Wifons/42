a3432249ca72ff733503642d553c040dafe45102
Wifons <williamfons6@gmail.com>
2024-11-08 18:45:50 +0100

    + ft_memmove

--- Cursus/libft/srcs/ft_memmove.c
+++ Cursus/libft/srcs/ft_memmove.c
@@ -1,29 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jewtwo <jewtwo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/08 15:26:44 by wifons            #+#    #+#             */
-/*   Updated: 2024/11/08 16:50:10 by jewtwo           ###   ########.fr       */
+/*   Created: 2024/11/08 16:34:14 by jewtwo            #+#    #+#             */
+/*   Updated: 2024/11/08 18:44:12 by jewtwo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned char	*dest_new;
+	unsigned char				*dest_new;
 	const unsigned char	*src_new;
-	size_t			i;
 
-	if (!dest && !src)
-		return (NULL);
-	dest_new = (unsigned char *)dest;
-	src_new = (const unsigned char *)src;
-	i = 0;
-	while (i++ < n)
-		*dest_new++ = *src_new++;
+	if (dest == src || n == 0)
+		return (dest);
+	if (dest < src)
+	{
+		dest_new = (unsigned char *)dest;
+		src_new = (const unsigned char *)src;
+		while (n--)
+			*dest_new++ = *src_new++;
+	}
+	else
+	{
+		dest_new = (unsigned char *)dest + (n - 1);
+		src_new = (const unsigned char *)src + (n - 1);
+		while (n--)
+			*dest_new-- = *src_new--;
+	}
 	return (dest);
 }
