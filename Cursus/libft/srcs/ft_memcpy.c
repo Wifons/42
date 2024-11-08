d314669104364f0ab85a3aa75019dd3261c753a8
William Fons <wifons@c2r4p2.42perpignan.fr>
2024-11-08 15:31:31 +0100

    + ft_memcpy

---
+++ Cursus/libft/srcs/ft_memcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wifons <wifons@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 15:26:44 by wifons            #+#    #+#             */
+/*   Updated: 2024/11/08 15:30:40 by wifons           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*new_dest;
+	unsigned char	*new_src;
+	size_t			i;
+
+	if (!dest && !src)
+		return (0);
+	new_dest = dest;
+	new_src = (unsigned char *)src;
+	i = 0;
+	while (i++ < n)
+		*new_dest++ = *new_src++;
+	return (dest);
+}
