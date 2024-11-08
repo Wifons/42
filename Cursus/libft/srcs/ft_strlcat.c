ea1772a93da9b71ab3370829132a053f3481c4c6
Wifons <williamfons6@gmail.com>
2024-11-08 19:14:00 +0100

    update

--- Cursus/libft/srcs/ft_strlcat.c
+++ Cursus/libft/srcs/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wifons <wifons@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jewtwo <jewtwo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 18:57:25 by wifons            #+#    #+#             */
-/*   Updated: 2024/11/08 19:02:33 by wifons           ###   ########.fr       */
+/*   Updated: 2024/11/08 19:12:57 by jewtwo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,17 @@ size_t ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	dst_len;
 	size_t	src_len;
 
-	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
+	if (!dst && size == 0)
+        return src_len;
+	dst_len = ft_strlen(dst);
 	i = 0;
-	if (size < dst_len + 1)
+	if (size <= dst_len)
 		return (size + src_len);
-	if (size > dst_len + 1)
+	while (src[i] && (dst_len + i) < (size - 1))
 	{
-		while (src[i] && dst_len + 1 + i < size)
-		{
-			dst[size + i] = src[i];
-			i++;
-		}
+		dst[dst_len + i] = src[i];
+		i++;
 	}
 	dst[dst_len + i] = '\0';
 	return (dst_len + src_len);
