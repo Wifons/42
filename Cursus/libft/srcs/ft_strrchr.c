f28aace9e39e4914647691c4adf6be204c26ff3f
Wifons <williamfons6@gmail.com>
2024-11-08 20:15:42 +0100

    Update

--- Cursus/libft/srcs/ft_strrchr.c
+++ Cursus/libft/srcs/ft_strrchr.c
@@ -3,14 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wifons <wifons@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jewtwo <jewtwo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 20:10:31 by wifons            #+#    #+#             */
-/*   Updated: 2024/11/08 20:11:53 by wifons           ###   ########.fr       */
+/*   Updated: 2024/11/08 20:14:09 by jewtwo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/ft.h"
+
 char	*ft_strrchr(const char *s, int c)
 {
-		
+	const char *last_occurence = NULL;
+
+	while (*s)
+	{
+		if (*s == (char)c)
+			last_occurence = s;
+		s++;
+	}	
+	if (c == '\0')
+		return (char *)s;
+	return (char *)last_occurence;
 }
