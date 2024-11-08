cfcfe629ab83ab5d07ba75a42ef2ae02132bb9c5
William Fons <wifons@c2r4p2.42perpignan.fr>
2024-11-08 15:39:20 +0100

    update ft_memcpy

--- Cursus/libft/srcs/ft_memcpy.c
+++ Cursus/libft/srcs/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: wifons <wifons@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 15:26:44 by wifons            #+#    #+#             */
-/*   Updated: 2024/11/08 15:37:16 by wifons           ###   ########.fr       */
+/*   Updated: 2024/11/08 15:39:13 by wifons           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char	*new_dest;
-	unsigned char	*new_src;
+	const unsigned char	*new_src;
 	size_t			i;
 
 	if ((dest == NULL || src == NULL) && n > 0)
